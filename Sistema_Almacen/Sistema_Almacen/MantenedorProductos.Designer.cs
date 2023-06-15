23467fa88f202f59b2a8d53ba3ae1262cfc80f66
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2023-06-15 17:04:47 -0500

    producto

--- Sistema_Almacen/Sistema_Almacen/MantenedorProductos.Designer.cs
+++ Sistema_Almacen/Sistema_Almacen/MantenedorProductos.Designer.cs
@@ -133,7 +133,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(605, 45);
+            button4.Location = new Point(605, 50);
             button4.Name = "button4";
             button4.Size = new Size(94, 29);
             button4.TabIndex = 10;
