76f63cd5a7c5aa48c7fb59aad6491ffad1a61492
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2023-06-19 12:01:01 -0500

    interfaz recepcion

--- Sistema_Almacen/Sistema_Almacen/RecepcionMercaderia.Designer.cs
+++ Sistema_Almacen/Sistema_Almacen/RecepcionMercaderia.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            ListViewItem listViewItem7 = new ListViewItem("Item");
-            ListViewItem listViewItem8 = new ListViewItem("Producto");
-            ListViewItem listViewItem9 = new ListViewItem("Cantidad");
+            ListViewItem listViewItem1 = new ListViewItem("Item");
+            ListViewItem listViewItem2 = new ListViewItem("Producto");
+            ListViewItem listViewItem3 = new ListViewItem("Cantidad");
             label1 = new Label();
             textBox1 = new TextBox();
             label2 = new Label();
@@ -43,12 +43,14 @@
             listView1 = new ListView();
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
+            dateTimePicker1 = new DateTimePicker();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(18, 92);
+            label1.Location = new Point(64, 63);
             label1.Name = "label1";
             label1.Size = new Size(61, 20);
             label1.TabIndex = 0;
@@ -56,7 +58,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(85, 89);
+            textBox1.Location = new Point(131, 60);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(125, 27);
             textBox1.TabIndex = 1;
@@ -64,15 +66,15 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(18, 132);
+            label2.Location = new Point(37, 100);
             label2.Name = "label2";
-            label2.Size = new Size(169, 20);
+            label2.Size = new Size(88, 20);
             label2.TabIndex = 2;
-            label2.Text = "Número de Documento:";
+            label2.Text = "Nro Factura:";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(193, 129);
+            textBox2.Location = new Point(131, 93);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(183, 27);
             textBox2.TabIndex = 3;
@@ -81,7 +83,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(18, 165);
+            label3.Location = new Point(84, 133);
             label3.Name = "label3";
             label3.Size = new Size(40, 20);
             label3.TabIndex = 4;
@@ -89,7 +91,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(64, 162);
+            textBox3.Location = new Point(130, 126);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(100, 27);
             textBox3.TabIndex = 5;
@@ -97,7 +99,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(170, 165);
+            label4.Location = new Point(27, 162);
             label4.Name = "label4";
             label4.Size = new Size(97, 20);
             label4.TabIndex = 6;
@@ -105,7 +107,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(273, 162);
+            textBox4.Location = new Point(131, 159);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(219, 27);
             textBox4.TabIndex = 7;
@@ -113,16 +115,16 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(110, 202);
+            label5.Location = new Point(198, 203);
             label5.Name = "label5";
-            label5.Size = new Size(266, 20);
+            label5.Size = new Size(148, 20);
             label5.TabIndex = 8;
-            label5.Text = "DETALLE RECEPCIÓN DE MERCADERÍA";
+            label5.Text = "DETALLE RECEPCIÓN";
             // 
             // listView1
             // 
-            listView1.Items.AddRange(new ListViewItem[] { listViewItem7, listViewItem8, listViewItem9 });
-            listView1.Location = new Point(18, 240);
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1, listViewItem2, listViewItem3 });
+            listView1.Location = new Point(18, 226);
             listView1.Name = "listView1";
             listView1.Size = new Size(474, 213);
             listView1.TabIndex = 9;
@@ -130,7 +132,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(398, 459);
+            button1.Location = new Point(398, 445);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 10;
@@ -139,18 +141,36 @@
             // 
             // button2
             // 
-            button2.Location = new Point(298, 459);
+            button2.Location = new Point(198, 445);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 11;
             button2.Text = "Regresar";
             button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Location = new Point(298, 445);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 12;
+            button3.Text = "Modificar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(213, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(289, 27);
+            dateTimePicker1.TabIndex = 13;
+            // 
             // RecepcionMercaderia
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(504, 518);
+            ClientSize = new Size(514, 499);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(listView1);
@@ -183,5 +203,7 @@
         private ListView listView1;
         private Button button1;
         private Button button2;
+        private Button button3;
+        private DateTimePicker dateTimePicker1;
     }
 }
