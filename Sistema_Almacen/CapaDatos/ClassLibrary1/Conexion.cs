7641114d41bb62bab2045fbe2cb5133620c25a80
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2023-06-20 06:53:07 -0500

    entTipoProducto

---
+++ Sistema_Almacen/CapaDatos/ClassLibrary1/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia { get { return Conexion._instancia; } }
+        public SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=DESKTOP-CD78G6K;Initial Catalog=Sistema_Almacen;Integrated Security=True";
+            return cn;
+        }
+    }
+}
---
+++ Sistema_Almacen/CapaEntidad/entTipoProducto.cs
@@ -0,0 +1,9 @@
+﻿namespace CapaEntidad
+{
+    public class entTipoProducto
+    {
+        public int CodtipPro { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
