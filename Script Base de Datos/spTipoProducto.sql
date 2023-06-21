fe07ae129fec576f64992e7ea7d2322c90495695
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2023-06-21 18:44:33 -0500

    Add files via upload

---
+++ Script Base de Datos/spTipoProducto.sql
@@ -0,0 +1,32 @@
+create procedure spRegistroTipoProducto(
+	@Nombre varchar(20),
+	@Descripcion varchar(20)
+) as
+begin
+insert into TipoProducto (Nombre, Descripcion) values (@Nombre, @Descripcion)
+end
+
+create procedure spActualizarTipoProducto(
+	@CodTipPro int,
+	@Nombre varchar(20),
+	@Descripcion varchar(60)
+)as
+begin
+update TipoProducto set
+	Nombre = @Nombre,
+	Descripcion = @Descripcion
+	where CodtipPro = @CodTipPro
+end
+
+create procedure spListaTipoProducto
+as
+	select CodTipPro, Nombre, Descripcion from TipoProducto
+
+create procedure spEliminarTipoProducto(
+	@CodTipPro int,
+	@Nombre varchar(20),
+	@Descripcion varchar(60)
+)as
+begin
+	delete from TipoProducto where CodTipPro = @CodTipPro
+end
